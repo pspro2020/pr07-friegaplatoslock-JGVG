a001ac29d6b776f456211a344ad7c565dd7f79cc
José Gabriel Vázquez Gil <josegabrielvazquezgil.saladillo@gmail.com>
2020-10-28 10:06:45 +0100

    Final commit

--- src/friegaplatos/Bandeja.java
+++ src/friegaplatos/Bandeja.java
@@ -8,11 +8,9 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Bandeja {
     ArrayList<Plato> bandeja_platos = new ArrayList();
-    int capacidad_bandeja = 10;
 
     private Lock reentrantLock = new ReentrantLock(true);
-    Condition notFull = reentrantLock.newCondition();
-    Condition notEmpty = reentrantLock.newCondition();
+    Condition conditionIsEmpty = reentrantLock.newCondition();
 
     public Bandeja( int num_platos){
         for(int i = 0; i<num_platos; i++){
@@ -26,11 +24,10 @@ public class Bandeja {
         try{
             while(bandeja_platos.isEmpty()){
                 System.out.println(nombre+" espera a que haya platos -> "+ LocalTime.now().getHour()+":"+LocalTime.now().getMinute()+":"+LocalTime.now().getSecond());
-                notEmpty.await();
+                conditionIsEmpty.await();
             }
             plato = bandeja_platos.remove(0);
             System.out.println(nombre+" ha cogido el "+plato.toString()+" -> "+ LocalTime.now().getHour()+":"+LocalTime.now().getMinute()+":"+LocalTime.now().getSecond());
-            notEmpty.signal();
             return plato;
         }finally {
             reentrantLock.unlock();
@@ -41,16 +38,11 @@ public class Bandeja {
     public void colocar(Plato plato, String nombre) throws InterruptedException {
         reentrantLock.lock();
         try{
-            while(bandeja_platos.size() >= capacidad_bandeja){
-                System.out.println(nombre+" no puede colocar más platos-> "+ LocalTime.now().getHour()+":"+LocalTime.now().getMinute()+":"+LocalTime.now().getSecond());
-                notFull.await();
-            }
             bandeja_platos.add(plato);
             System.out.println(nombre + " ha colocado el " +plato.toString()+" en bandeja -> "+ LocalTime.now().getHour()+":"+LocalTime.now().getMinute()+":"+LocalTime.now().getSecond());
-            notFull.signal();
+            conditionIsEmpty.signal();
         }finally {
             reentrantLock.unlock();
         }
-
     }
 }
